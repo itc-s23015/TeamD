b6108800219e2dc562c72dc7aed3aeb1b9c51472
itc-s23015 <s23015@std.it-college.ac.jp>
2024-12-10 03:01:42 +0000

    回答者画面完成

--- src/app/components/answer.js
+++ src/app/components/answer.js
@@ -4,38 +4,41 @@ import styles from "../styles/answer.module.css";
 
 const Answer = () => {
   const router = useRouter();
+  const [answer, setAnswer] = useState(""); 
+  const [lives, setlives] = useState(5); // ライフ
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [message, setMessage] = useState(""); // メッセージ表示用
 
-  const [answer, setAnswer] = useState("");
-  const [lives, setlives] = useState(5);
-  
-  // 本来の答えを入れるようにする
+  // 仮の回答
   const correctAnswer = "test";
 
   const handleSubmit = (e) => {
     e.preventDefault();
   
 
-  if (lives <= 0) return;
+  if (isCorrect || lives <= 0) return;
 
   // 正解の場合
   if (answer.trim() === correctAnswer) {
-    alert("正解!");
-    setAnswer("")
+    setMessage("正解!");
+    setIsCorrect(true);
     return
   }
 
     //不正解の場合
     setlives((prev) => {
       const lives = Math.max(prev - 1, 0);
-      alert(lives > 0 ? "間違いです！": "ライフがなくなりました");
+      setMessage(lives > 0 ? "間違いです！": "ライフがなくなりました");
       return lives;
     });
-    setAnswer("");
   };
 
 
   return (
     <div className={styles.container}>
+      <div className={styles.messageContainer}>
+        {message && <p className={styles.message}>{message}</p>}
+      </div>
       <h1>これは何でしょう？</h1>
       <div className={styles.imageWrapper}>
       </div>
@@ -49,9 +52,10 @@ const Answer = () => {
             onChange={(e) => setAnswer(e.target.value)}
             placeholder="ひらがな・カタカナのみ入力可能"
             className={styles.input}
+            disabled={isCorrect || lives <= 0} // 正解、ライフ０で入力不可
           />
 
-          <button type="submit" className={styles.button}>
+          <button type="submit" className={styles.button} disabled={isCorrect || lives <= 0}>
             回答
           </button>
         </div>
