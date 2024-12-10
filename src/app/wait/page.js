270f1cc28bcadb18d694fb12975ee12e56552ed7
its-s23028 <s23028@std.it-college.ac.jp>
2024-12-10 01:28:26 +0000

    待機画面のUI完成

---
+++ src/app/wait/page.js
@@ -0,0 +1,12 @@
+'use client'; // クライアントコンポーネントとして指定
+
+import React from 'react';
+import Wait from '../components/wait';
+
+export default function WaitPage() {
+  return (
+    <div>
+      <Wait />
+    </div>
+  );
+}
